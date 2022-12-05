75bb545784f6cf7e1b8194214f1b37236065fb21
Archicoders-Rein <113080949+Archicoders-Rein@users.noreply.github.com>
2022-12-05 09:06:12 +0800

    Update functions.php

--- Slick Slider/functions.php
+++ Slick Slider/functions.php
@@ -1,8 +1,7 @@
-<?php
-	function initSlickSlider() {
-		wp_enqueue_style( 'slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css' );
-		wp_enqueue_style( 'slick-theme-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css' );
-		wp_enqueue_script( 'slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery') );
-	}
-	add_action( 'wp_enqueue_scripts', 'initSlickSlider' );
+<?php 
+	add_action('wp_enqueue_scripts', function(){
+		wp_enqueue_style('slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css');
+		wp_enqueue_style('slick-theme-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css');
+		wp_enqueue_script('slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'));
+	});
 ?>
