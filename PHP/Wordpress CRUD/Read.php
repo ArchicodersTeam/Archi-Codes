148f8cb48bb9a073faec77786eae015c02fae4e3
Archicoders-Rein <113080949+Archicoders-Rein@users.noreply.github.com>
2023-03-09 17:09:26 +0800

    Update Read.php

--- PHP/Wordpress CRUD/Read.php
+++ PHP/Wordpress CRUD/Read.php
@@ -1,38 +1,37 @@
 <?php
 
-function get_hp_listing_posts( $request ) {
-    $posts_per_page = $request->get_param( 'posts_per_page' ) !== null && $request->get_param( 'posts_per_page' ) !== '' ? $request->get_param( 'posts_per_page' ) : 10;
-    $page = $request->get_param( 'page' ) !== null && $request->get_param( 'page' ) !== '' ? $request->get_param( 'page' ) : 1;
-      
-    $offset = ( $page - 1 ) * $posts_per_page;
-      
-    $args = array(
-      'post_type' => 'hp_listing',
-      'posts_per_page' => $posts_per_page,
-      'offset' => $offset
-    );
-  
-    $query = new WP_Query( $args );
-    $posts = $query->get_posts();
-  
-    if ( empty( $posts ) ) {
-      return new WP_Error( 'empty_posts', 'No posts found', array( 'status' => 404 ) );
-    }
-  
-    $total_posts = $query->found_posts;
-    $total_pages = ceil( $total_posts / $posts_per_page );
-  
-    $data = array(
-      'total_posts' => $total_posts,
-      'total_pages' => $total_pages,
-      'current_page' => $page,
-      'posts' => array()
-    );
-      
-  
-    foreach ( $posts as $post ) {
-  
-       $post_terms = wp_get_post_terms( $post->ID, 'hp_vendor_category' ); // replace 'taxonomy_name' with the actual taxonomy name
+    function get_hp_listing_posts( $request ) {
+      $posts_per_page = $request->get_param( 'posts_per_page' ) !== null && $request->get_param( 'posts_per_page' ) !== '' ? $request->get_param( 'posts_per_page' ) : 10;
+      $page = $request->get_param( 'page' ) !== null && $request->get_param( 'page' ) !== '' ? $request->get_param( 'page' ) : 1;
+
+      $offset = ( $page - 1 ) * $posts_per_page;
+
+      $args = array(
+        'post_type' => 'hp_listing',
+        'posts_per_page' => $posts_per_page,
+        'offset' => $offset
+      );
+
+      $query = new WP_Query( $args );
+      $posts = $query->get_posts();
+
+      if ( empty( $posts ) ) {
+        return new WP_Error( 'empty_posts', 'No posts found', array( 'status' => 404 ) );
+      }
+
+      $total_posts = $query->found_posts;
+      $total_pages = ceil( $total_posts / $posts_per_page );
+
+      $data = array(
+        'total_posts' => $total_posts,
+        'total_pages' => $total_pages,
+        'current_page' => $page,
+        'posts' => array()
+      );
+
+
+      foreach ( $posts as $post ) {
+        $post_terms = wp_get_post_terms( $post->ID, 'hp_listing_category' ); // replace 'hp_listing_category' with the actual taxonomy name
         $terms_array = array();
         foreach ( $post_terms as $term ) {
           $terms_array[] = array(
@@ -49,18 +48,20 @@ function get_hp_listing_posts( $request ) {
           'permalink' => get_permalink( $post->ID ),
           'terms' => $terms_array
         );
-  }
-  
-    return rest_ensure_response( $data );
-  }
-  
-  function register_hp_listing_endpoint() {
-      register_rest_route( 'job_listings/v1', '/posts', array(
-          'methods' => 'GET',
-          'callback' => 'get_hp_listing_posts'
-      ));
-  }
-  
-  add_action( 'rest_api_init', 'register_hp_listing_endpoint' );  
+      }
+
+      return rest_ensure_response( $data );
+    }
+
+
+    function register_hp_listing_endpoint() {
+        register_rest_route( 'job_listings/v1', '/posts', array(
+            'methods' => 'GET',
+            'callback' => 'get_hp_listing_posts'
+        ));
+    }
+
+    add_action( 'rest_api_init', 'register_hp_listing_endpoint' );
+
 
 ?>
